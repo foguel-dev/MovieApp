a73da49d86f71f3604e6f5c78bd81827558cfac4
Yamil Vogl <yamil.vogl@regxi.com>
2021-07-26 12:08:37 -0600

    dependencias y primeros layouts

---
+++ app/src/main/java/com/example/movieapp/DetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/movieapp/MoviesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+
+class MoviesFragment : Fragment(R.layout.fragment_movies) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
