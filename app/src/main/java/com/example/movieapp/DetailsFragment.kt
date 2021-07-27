68f6be2f6745f9216acea304907767262ee93acc
Yamil Vogl <yamil.vogl@regxi.com>
2021-07-27 11:46:38 -0600

    recursos & viewbinding

--- app/src/main/java/com/example/movieapp/DetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/DetailsFragment.kt
@@ -5,17 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.movieapp.databinding.FragmentDetailsBinding
 
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    //Referencia a las vistas del layouys
+    private lateinit var binding: FragmentDetailsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailsBinding.bind(view)
     }
 
 }
--- app/src/main/java/com/example/movieapp/MoviesFragment.kt
+++ app/src/main/java/com/example/movieapp/MoviesFragment.kt
@@ -3,17 +3,17 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.example.movieapp.databinding.FragmentMoviesBinding
 
 
 class MoviesFragment : Fragment(R.layout.fragment_movies) {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentMoviesBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMoviesBinding.bind(view)
+
     }
 
 }
