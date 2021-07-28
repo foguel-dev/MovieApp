18ca88606f9ab514fd41a0e408c3daf23f6b812b
Yamil Vogl <yamil.vogl@regxi.com>
2021-07-28 16:28:57 -0600

    capa presentation

--- app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.movieapp.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.movieapp.core.Resource
+import com.example.movieapp.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+class MovieViewModel(private val repo:MovieRepository): ViewModel() {
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO){
+        //Informacion se esta cargando
+        //Emitar datos a la vista
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getUpcomingMovies()))
+        }catch (e: Exception){
+            emit(Resource.Error(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repo:MovieRepository):ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
