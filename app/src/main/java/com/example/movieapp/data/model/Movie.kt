8b102744e07965bf585d39f1fcc08ada5dd70b40
Yamil Vogl <yamil.vogl@regxi.com>
2021-07-28 11:26:17 -0600

    capa de datos

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,4 +1,20 @@
 package com.example.movieapp.data.model
 
-data class Movie(val id: Int = -1){
-}
+data class Movie(
+    val id: Int = -1,
+    val adult: Boolean,
+    val backdrop_path: String = "",
+    val genre_ids: List<Int> = listOf(),
+    val original_language: String = "",
+    val original_title: String = "",
+    val overview: String = "",
+    val popularity: String = "",
+    val poster_path: String = "",
+    val release_date: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val vote_average: Double = -1.0,
+    val vote_count: Int = -1
+    )
+
+data class MovieList(val results: List<Movie> = listOf())
--- app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,19 @@
 package com.example.movieapp.data.remote
 
+import com.example.movieapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies():MovieList{
+        return MovieList()
+    }
+
+    fun getTopRateMovies():MovieList{
+        return MovieList()
+    }
+
+    fun getPopularMovies():MovieList{
+        return MovieList()
+    }
+
 }
