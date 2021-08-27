8f882da3bdcb84d9a01877935b62b5eb61c8f0dd
Yamil Vogl <yamil.vogl@regxi.com>
2021-08-27 11:23:53 -0600

    cambios

--- app/src/main/java/com/example/movieapp/application/AppConstants.kt
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -3,6 +3,6 @@ package com.example.movieapp.application
 object AppConstants {
 
     //Base de la API & API KEY
-    val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     val API_KEY = "8bbf7ad400be843aee2c1f21ba536043"
 }
